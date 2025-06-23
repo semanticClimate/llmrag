fc4a6fc69b94dcd8952098f7f5fb44f01c892e71
petermr <peter.murray.rust@googlemail.com>
2025-06-23 19:28:24 +0100

    add IPCC chunks

--- tests/test_chunking.py
+++ tests/test_chunking.py
@@ -1,5 +1,10 @@
 import unittest
+from pathlib import Path
+
+from lxml.html import HTMLParser
+
 from llmrag.chunking import split_documents
+from lxml import etree as ET
 
 class TestChunking(unittest.TestCase):
     def test_split_documents(self):
@@ -7,3 +12,30 @@ class TestChunking(unittest.TestCase):
         chunks = split_documents(text, chunk_size=10, overlap=2)
         self.assertEqual(chunks[0], "abcdefghij")
         self.assertEqual(chunks[1][:2], "ij")  # Overlap check
+
+    def test_split_html_with_ids(self):
+        test_dir = Path("tests", "ipcc")
+        assert test_dir.exists(), f"{test_dir} should exist"
+        wg = "wg1"
+        chapter = "chapter04"
+        div_id = "4.1"
+        p_count = 12
+        chunks = self.create_chunks(test_dir, wg, chapter, div_id, p_count)
+        for chunk in chunks:
+            print(f"{len(chunk)}: {chunk[:200]}")
+
+    def create_chunks(self, dir, wg, chapter, div_id, p_count=None):
+        chap_html = Path(dir, wg, chapter, "html_with_ids.html")
+        assert chap_html.exists(), f"chapter {chap_html} should exist"
+        htmlx = ET.parse(chap_html, HTMLParser())
+        assert htmlx is not None
+        div4_1 = htmlx.find(f".//div[@id='{div_id}']")
+        assert div4_1 is not None
+        paras = div4_1.findall("./div/p[@id]")
+        if p_count:
+            assert (n := len(paras)) == p_count, f"expected {p_count} paras, found {n}"
+        chunks = []
+        for para in paras:
+            text = " ".join(para.itertext())
+            chunks.append(text)
+        return chunks
